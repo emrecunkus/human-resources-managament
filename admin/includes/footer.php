94a9de787400ccdb98e176d4789ee2a2080cb443
emre2023 <muhammed8336@gmail.com>
2021-09-26 00:53:43 +0300

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b><a href="#">İnsan Kaynakları Yönetim Sistemi</a></b>
+    </div>
+    <strong>Copyright &copy; 2021 HRM Tüm hakları Muhammed Emre Çunkuş'a aittir. </strong>
+</footer>
